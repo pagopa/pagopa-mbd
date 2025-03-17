281f9868c5603d7beb867aa4adb92249019369be
svariant <samuele.varianti@nttdata.com>
2025-03-17 12:28:40 +0100

    feat: [PAGOPA-2750] Implement workload identity

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -17,9 +17,14 @@ data "github_organization_teams" "all" {
   summary_only    = true
 }
 
-data "azurerm_user_assigned_identity" "identity_cd_01" {
-  name                = "${local.prefix}-${var.env_short}-${local.domain}-01-github-cd-identity"
-  resource_group_name = "${local.prefix}-${var.env_short}-identity-rg"
+data "azurerm_user_assigned_identity" "identity_cd" {
+  resource_group_name = "${local.product}-identity-rg"
+  name                = "${local.product}-${local.domain}-job-01-github-cd-identity"
+}
+
+data "azurerm_user_assigned_identity" "workload_identity_clientid" {
+  name                = "mbd-workload-identity"
+  resource_group_name = "pagopa-${var.env_short}-${var.location_short}-${var.env}-aks-rg"
 }
 
 data "azurerm_key_vault" "key_vault" {
