f0771a68a37d6340e35df9209f0cfba2bc7e9bb8
svariant <samuele.varianti@nttdata.com>
2025-03-18 16:19:41 +0100

    fix: [PAGOPA-2750] Fix clientId & location in identity

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -24,7 +24,7 @@ data "azurerm_user_assigned_identity" "identity_cd" {
 
 data "azurerm_user_assigned_identity" "workload_identity_clientid" {
   name                = "mbd-workload-identity"
-  resource_group_name = "pagopa-${var.env_short}-${var.location_short}-${var.env}-aks-rg"
+  resource_group_name = "pagopa-${var.env_short}-weu-${var.env}-aks-rg"
 }
 
 data "azurerm_key_vault" "key_vault" {
