21937059548eda75ed825b08e6ed895224cfcb83
Federico Ruzzier <federicoruzzier96@gmail.com>
2025-09-11 17:21:24 +0200

    chore: add slack webhook to python script PAGOPA-3240

--- scripts/alert/run_alert_cronjobs.py
+++ scripts/alert/run_alert_cronjobs.py
@@ -3,6 +3,8 @@ import argparse
 import logging
 import datetime
 from zoneinfo import ZoneInfo
+import requests
+import sys
 
 # Configurazione del logging
 logging.basicConfig(
@@ -16,16 +18,30 @@ def parse_arguments():
     """Parse command line arguments"""
     parser = argparse.ArgumentParser(description='Extract mbd events from Azure storage account')
 
-    # Parametri per connessione a Azure storage account
+    # Parametro per connessione a Azure storage account
     parser.add_argument('--storage_conn_string', required=True,
                         help='Storage account connection string')
 
+    # Parametro webhook per slack
+    parser.add_argument('--slack-webhook', required=True,
+                        help='Slack channel webhook')
+
     return parser.parse_args()
+
+def format_bullet_list_from_set(item_set):
+    return '\n'.join([f"• *{item}*" for item in item_set])
+
+def send_slack_alert(url, payload):
+    response = requests.post(url, json = payload)
+    if not response.ok:
+        logger.error("Error sending message to slack via webhook")
+        sys.exit(1)
+
 
 def get_datetime_one_week():
     return datetime.datetime.now(ZoneInfo("Europe/Rome")) - datetime.timedelta(days=7)
 
-def alert_file_da_predisporre(file_service, share_name, directory_predisporre_name):
+def alert_file_da_predisporre(file_service, share_name, directory_predisporre_name, slack_webhook):
     files_predisporre_backup = file_service.list_directories_and_files(share_name, directory_predisporre_name)
     time_one_week = get_datetime_one_week()
 
@@ -36,14 +52,29 @@ def alert_file_da_predisporre(file_service, share_name, directory_predisporre_na
             if file.properties.last_modified > time_one_week:
                 break
     else:
-        logger.error("ALERT HERE")
+        payload = {
+            "blocks": [
+                    {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*ALERT: Nessun file da predisporre negli ultimi 7 giorni*"
+                        }
+                    }
+                ],
+            "username": "RMBD-Alert",
+            "icon_emoji": ":sos:"
+        }
+        send_slack_alert(slack_webhook, payload)
+        logger.warning("ALERT FILE PREDISPORRE")
 
-def alert_file_da_inviare(file_service, share_name, directory_predisporre_name, directory_inviare_name):
+def alert_file_da_inviare(file_service, share_name, directory_predisporre_name, directory_inviare_name, slack_webhook):
     files_predisporre_backup = file_service.list_directories_and_files(share_name, directory_predisporre_name)
     files_inviare_backup = file_service.list_directories_and_files(share_name, directory_inviare_name)
     time_one_week = get_datetime_one_week()
 
     file_da_inviare_list_last_week = set()
+    file_da_predisporre_non_presenti = set()
 
     for file_da_inviare in files_inviare_backup:
         if isinstance(file_da_inviare, File):
@@ -55,21 +86,46 @@ def alert_file_da_inviare(file_service, share_name, directory_predisporre_name, 
     for file_da_predisporre in files_predisporre_backup:
         if isinstance(file_da_predisporre, File):
             if not file_da_predisporre.name in file_da_inviare_list_last_week:
-                logger.error("ALERT HERE")
+                file_da_predisporre_non_presenti.add(file_da_predisporre.name)
+
+    if len(file_da_predisporre_non_presenti) > 0:
+        payload = {
+                    "blocks": [
+                            {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": "*ALERT: Lista dei file da predisporre non inviati nella scorsa settimana:*"
+                                }
+                            },
+                            {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": f"{format_bullet_list_from_set(file_da_predisporre_non_presenti)}"
+                                }
+                            }
+                        ],
+                    "username": "RMBD-Alert",
+                    "icon_emoji": ":sos:"
+                }
+        logger.warning("ALERT FILE INVIARE")
+        send_slack_alert(slack_webhook, payload)
 
 def main():
 
     args = parse_arguments()
     conn_string_storage = args.storage_conn_string
+    slack_webhook = args.slack_webhook
 
     file_service = FileService(connection_string=conn_string_storage)
     share_name = "firmatore"
     directory_predisporre_name = "backup/SID-Flussi-e-bollo/SID_cartelle/file_da_predisporre"
     directory_inviare_name = "backup/SID-Flussi-e-bollo/SID_cartelle/file_da_inviare"
 
-    alert_file_da_predisporre(file_service, share_name, directory_predisporre_name)
+    alert_file_da_predisporre(file_service, share_name, directory_predisporre_name, slack_webhook)
 
-    alert_file_da_inviare(file_service, share_name, directory_predisporre_name, directory_inviare_name)
+    alert_file_da_inviare(file_service, share_name, directory_predisporre_name, directory_inviare_name, slack_webhook)
 
     logger.info("Operation completed successfully")
 
