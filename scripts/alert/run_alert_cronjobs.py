78c8bdb8e43e9772b113b65e73ecef8716903c3a
Federico Ruzzier <federicoruzzier96@gmail.com>
2025-09-12 13:06:22 +0200

    chore: adding some correction to script PAGOPA-3240

--- scripts/alert/run_alert_cronjobs.py
+++ scripts/alert/run_alert_cronjobs.py
@@ -42,16 +42,24 @@ def get_datetime_one_week():
     return datetime.datetime.now(ZoneInfo("Europe/Rome")) - datetime.timedelta(days=7)
 
 def alert_file_da_predisporre(file_service, share_name, directory_predisporre_name, slack_webhook):
-    files_predisporre_backup = file_service.list_directories_and_files(share_name, directory_predisporre_name)
+    try:
+        files_predisporre_backup = file_service.list_directories_and_files(share_name, directory_predisporre_name)
+    except Exception as e:
+        logger.error(f"Error retrieving files in \"file_da_predisporre\" directory")
+        sys.exit(1)
+
     time_one_week = get_datetime_one_week()
+    recent_file_found = False
 
     for file_da_predisporre in files_predisporre_backup:
         if isinstance(file_da_predisporre, File):
             file_name = file_da_predisporre.name
             file = file_service.get_file_properties(share_name, directory_predisporre_name, file_name, timeout=None, snapshot=None)
             if file.properties.last_modified > time_one_week:
+                recent_file_found = True
                 break
-    else:
+
+    if not recent_file_found:
         payload = {
             "blocks": [
                     {
@@ -69,8 +77,13 @@ def alert_file_da_predisporre(file_service, share_name, directory_predisporre_na
         logger.warning("ALERT FILE PREDISPORRE")
 
 def alert_file_da_inviare(file_service, share_name, directory_predisporre_name, directory_inviare_name, slack_webhook):
-    files_predisporre_backup = file_service.list_directories_and_files(share_name, directory_predisporre_name)
-    files_inviare_backup = file_service.list_directories_and_files(share_name, directory_inviare_name)
+    try:
+        files_predisporre_backup = file_service.list_directories_and_files(share_name, directory_predisporre_name)
+        files_inviare_backup = file_service.list_directories_and_files(share_name, directory_inviare_name)
+    except Exception as e:
+        logger.error(f"Error retrieving files in \"file_da_predisporre\" and \"file_da_inviare\" directory")
+        sys.exit(1)
+
     time_one_week = get_datetime_one_week()
 
     file_da_inviare_list_last_week = set()
